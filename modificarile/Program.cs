ad5e1e087b5b95232af02e002cbba220b4a484f6
calugareanu.gloria@gmail.com <calugareanu.gloria@gmail.com>
2020-10-08 14:11:07 -0700

    Modificarile efectuate in program

---
+++ modificarile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Adelina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buna ziua");
+            Console.WriteLine("Introduceti nr de pagini a cartii: ");
+            int numarPagini;
+            try
+            {
+                numarPagini = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Cartea are {numarPagini} pagini");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Va rugam sa folositi doar numere intregi!");
+                return;
+            }
+
+
+
+        }
+    }
+}
